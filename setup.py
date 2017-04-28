c4195b7643f08989261186768b38fb5ae92a06d1
Hugo Osvaldo Barrera <hugo@barrera.io>
2017-04-28 15:33:37 -0300

    Add classifiers for missing pythons

--- setup.py
+++ setup.py
@@ -98,6 +98,12 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: PyPy',
         'Topic :: Software Development :: Object Brokering',
         'Topic :: Software Development :: Distributed Computing'
     ]
